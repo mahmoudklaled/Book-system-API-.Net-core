5963ec585ca7c45982c35b5dfc31c2cfc078aae5
mahmoudklaled <mahmoud.khaled19640@gmail.com>
2022-12-18 02:32:02 +0200

    First

---
+++ my-books/Controllers/PublishersController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        private PublishersService _publishersService;
+        public PublishersController(PublishersService publishersService)
+        {
+            _publishersService = publishersService;
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddBook([FromBody] PublisherVM publisher)
+        {
+            var newPublisher = _publishersService.AddPublisher(publisher);
+            return Created(nameof(AddBook), newPublisher);
+        }
+
+        [HttpGet("get-publisher-by-id/{id}")]
+        public IActionResult GetPublisherById(int id)
+        {
+            var _response = _publishersService.GetPublisherById(id);
+
+            if(_response != null)
+            {
+                return Ok(_response);
+            } else
+            {
+                return NotFound();
+            }
+        }
+
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _response = _publishersService.GetPublisherData(id);
+            return Ok(_response);
+        }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            _publishersService.DeletePublisherById(id);
+            return Ok();
+        }
+    }
+}
