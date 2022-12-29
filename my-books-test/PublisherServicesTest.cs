eabf4b53f092ca1195919180a2cf61f7db3ab3eb
mahmoudklaled <mahmoud.khaled19640@gmail.com>
2022-12-29 13:36:50 +0200

    1-unitTest

---
+++ my-books-test/PublisherServicesTest.cs
@@ -0,0 +1,16 @@
+namespace my_books_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
